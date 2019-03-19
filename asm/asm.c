59d729e41e9e5d9723741b5f0bddca3c4f6c9c00
Vladyslav Vereta <vereta.vlad@gmail.com>
2019-03-19 22:30:58 +0200

    Commands list.

--- asm/asm.c
+++ asm/asm.c
@@ -92,9 +92,15 @@ int		check_line(char **line, t_asm *asm_struct)
 	int 	i;
 
 	if (!asm_struct->header.name[0] && !get_substr_index(*line, ".name"))
+	{
 		get_champs_name(*line + 5, asm_struct);
+		return (1);
+	}
 	else if (!asm_struct->header.description[0] && !get_substr_index(*line, ".comment"))
+	{
 		get_champs_description(*line + 8, asm_struct);
+		return (1);
+	}
 	i = 0;
 	while (ft_strchr(LABEL_CHARS, (*line)[i++]))
 	{
@@ -113,6 +119,17 @@ int		check_line(char **line, t_asm *asm_struct)
 
 int 	check_for_command(char **line, t_asm *asm_struct, int start)
 {
+	int 	i = 0;
+	char	*command;
+
+	command = ft_strnew(10);
+//	ft_printf("%s\n", *line);
+	while ((*line)[start] == ' ' || (*line)[start] == '\t' || (*line)[start] == '\n' || (*line)[start] == '\0')
+		start++;
+	while (ft_islower((*line)[start]))
+		command[i++] = (*line)[start++];
+	ft_printf("%s\n", command);
+	ft_strdel(&command);
 	ft_strdel(line);
 	return (1);
 }
