c9a65777f2158ce5fa8f6fb1de28e83c60f6a11a
vlvereta <vlvereta.92@gmail.com>
2019-06-03 18:48:05 +0300

    Fix CMakeLists.txt for corewar execution.

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -47,5 +47,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/corewar.dir/DependInfo.cmake"
   "CMakeFiles/asm.dir/DependInfo.cmake"
   )
